18fa634aeb3f9662b72e78976ef58d8b3afdb47f
mhudaky <hudaky.marton@gmail.com>
2024-08-24 18:46:45 +0200

    nit

--- app/src/main/java/com/mhudaky/quizapp/utils/ColorUtil.kt
+++ app/src/main/java/com/mhudaky/quizapp/utils/ColorUtil.kt
@@ -6,19 +6,20 @@ import androidx.core.graphics.ColorUtils
 class ColorUtil {
     companion object {
 
-        val lightGrey = Color.parseColor("#D3D3D3")
-        val ORANGE = Color.parseColor("#FFAC1C")
+        val LIGHT_GREY = Color.parseColor("#D3D3D3")
+        val ORANGE = Color.parseColor("#FFA500")
+        val RED = Color.parseColor("#8B0000")
         fun dark(color: Int, darkening: Float): Int {
             return ColorUtils.blendARGB(color, Color.BLACK, darkening)
         }
 
         fun getColorFromInt(value: Int): Int {
             return when {
-                value <= 0 -> lightGrey
-                value in 1..24 -> ColorUtils.blendARGB(lightGrey, Color.GREEN, value / 25f)
+                value <= 0 -> LIGHT_GREY
+                value in 1..24 -> ColorUtils.blendARGB(LIGHT_GREY, Color.GREEN, value / 25f)
                 value in 25..49 -> ColorUtils.blendARGB(Color.GREEN, ORANGE, (value - 25) / 25f)
-                value in 50..74 -> ColorUtils.blendARGB(ORANGE, Color.RED, (value - 50) / 25f)
-                value in 75..99 -> ColorUtils.blendARGB(Color.RED, Color.MAGENTA, (value - 75) / 25f)
+                value in 50..74 -> ColorUtils.blendARGB(ORANGE, RED, (value - 50) / 25f)
+                value in 75..99 -> ColorUtils.blendARGB(RED, Color.MAGENTA, (value - 75) / 25f)
                 else -> Color.MAGENTA
             }
         }
