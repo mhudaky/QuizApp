c307d771da57c7cc5f937a43c89fcaa491795986
mhudaky <hudaky.marton@gmail.com>
2024-08-24 14:10:25 +0200

    modify colors

--- app/src/main/java/com/mhudaky/quizapp/enums/Difficulty.kt
+++ app/src/main/java/com/mhudaky/quizapp/enums/Difficulty.kt
@@ -1,13 +1,11 @@
 package com.mhudaky.quizapp.enums
 
-import android.graphics.Color.GREEN
-import android.graphics.Color.MAGENTA
-import android.graphics.Color.RED
-import android.graphics.Color.YELLOW
+import android.graphics.Color
+import androidx.core.graphics.ColorUtils
 
 enum class Difficulty(val value: String, val displayColor: Int) {
-    EASY("Easy", GREEN),
-    MEDIUM("Medium", YELLOW),
-    HARD("Hard", RED),
-    EXPERT("Expert", MAGENTA);
+    EASY("Easy", ColorUtils.blendARGB(Color.GREEN, Color.BLACK, 0.5f)),
+    MEDIUM("Medium", ColorUtils.blendARGB(Color.YELLOW, Color.BLACK, 0.5f)),
+    HARD("Hard", ColorUtils.blendARGB(Color.RED, Color.BLACK, 0.5f)),
+    EXPERT("Expert", ColorUtils.blendARGB(Color.MAGENTA, Color.BLACK, 0.5f));
 }
